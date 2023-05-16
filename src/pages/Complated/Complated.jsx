dfada3468f0fc6ec557579df238340000f88df09
unknown <ilia.chkoidze12@gmail.com>
2023-05-16 15:19:03 +0400

    style:change flex containers size to 48% and 52%

--- src/pages/Complated/Complated.jsx
+++ src/pages/Complated/Complated.jsx
@@ -6,15 +6,15 @@ import rocket from "../../assets/Succsess rocket.jpg";
 export default function Complated() {
   return (
     <div className="flex ">
-      <div className="w-1/2 ">
+      <div className="w-[48%] ">
         <div className="bg-[#7025FB] py-[30px] pl-[60px]">
           <img src={crown} alt="" />
         </div>
         <img src={backBoard} alt="" />
       </div>
-      <div className="w-1/2 flex flex-col items-center justify-center">
+      <div className="w-[52%] flex flex-col items-center justify-center">
         <img src={rocket} alt="" />
-        <p className="text-[36px] font-nunito text-[#000] leading-[150%] mt-[16px]">
+        <p className="text-[36px] font-nunito font-extrabold not-italic text-[#000] leading-[150%] mt-[16px]">
           Onboarding completed!
         </p>
       </div>
