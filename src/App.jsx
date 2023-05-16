a2769f5d62d36ef6ef8346c9d5b95b075882082f
lashasuxa <lashasukhashvili1995@gmail.com>
2023-05-16 16:11:47 -0400

    feat:routes for pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import Complated from "./pages/Complated/Complated";
 
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Completed from './pages/Completed/Completed';
+import Experience from './pages/Experience/Experience';
+import Landing from './pages/Landing/Landing';
+import Personal from './pages/Personal/Personal';
+
+export default function App() {
   return (
-    <>
-      <Complated></Complated>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/completed" element={<Completed />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/personal" element={<Personal />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
