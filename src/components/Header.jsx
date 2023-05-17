02a01a28e93f2672f055dc3aecf0c05f7aeaa629
unknown <ilia.chkoidze12@gmail.com>
2023-05-17 09:43:47 +0400

    style:add blue line and logo to the Header file

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HeaderLogo from "../../assets/Knight cup logo.svg";
+
+function Header() {
+  return (
+    <div className="bg-[#7025FB] py-[30px] pl-[60px]">
+      <img src={HeaderLogo} alt="knight cup logo" />
+    </div>
+  );
+}
+
+export default Header;
